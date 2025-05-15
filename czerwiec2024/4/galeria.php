8c9dd4c73b212ec2e85f010f229cff5fc36c3d18
Bartosz <bartoszbachul1@gmail.com>
2025-05-15 14:59:02 +0200

    struktura strony i style

---
+++ czerwiec2024/4/galeria.php
@@ -0,0 +1,55 @@
+<?php
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Galeria</title>
+
+    <link rel="stylesheet" href="styl.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>Tematy zdjęć</h1>
+    </header>
+
+    <main>
+        <aside>
+            <h2>Tematy zdjęć</h2>
+
+            <ul>
+                <li>Zwierzęta</li>
+                <li>Krajobrazy</li>
+                <li>Miasta</li>
+                <li>Przyroda</li>
+                <li>Samochody</li>
+            </ul>
+        </aside>
+
+        <article>
+
+        </article>
+
+        <aside>
+            <h2>Najbardziej lubiane</h2>
+
+            <strong>Zobacz wszystkie nasze zdjęcia</strong>
+        </aside>
+    </main>
+
+    <footer>
+        <p>Stronę wykonał: 12345678910</p>
+    </footer>
+
+</body>
+
+</html>
