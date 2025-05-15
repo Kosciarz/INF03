42d4c8007b7742cff517f87ff5b9e4c6c46827cd
Bartosz <bartoszbachul1@gmail.com>
2025-05-15 15:24:57 +0200

    Zapytania w PHP i wyświetlenie informacji

--- czerwiec2024/4/galeria.php
+++ czerwiec2024/4/galeria.php
@@ -1,7 +1,21 @@
 <?php
 
+$conn = mysqli_connect('localhost', 'root', '', 'galeria');
+if (!$conn)
+    die("Nie udało się połączyć z bazą danych: " . mysqli_connect_error());
 
+$query1 = "SELECT zdjecia.plik, zdjecia.tytul, zdjecia.polubienia, autorzy.imie, autorzy.nazwisko
+FROM zdjecia
+JOIN autorzy ON autorzy.id = zdjecia.autorzy_id
+ORDER BY autorzy.nazwisko";
+$zdjecia = mysqli_query($conn, $query1);
 
+$query2 = "SELECT zdjecia.tytul, zdjecia.plik
+FROM zdjecia
+WHERE zdjecia.polubienia >= 100";
+$najbardziej_lubiane = mysqli_fetch_row(mysqli_query($conn, $query2));
+
+mysqli_close($conn);
 
 ?>
 
@@ -10,7 +24,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Galeria</title>
 
     <link rel="stylesheet" href="styl.css">
@@ -37,10 +51,27 @@
 
         <article>
 
+            <?php while ($zdjecie = mysqli_fetch_row($zdjecia)): ?>
+                <div class="zdjecie">
+                    <img src="<?= $zdjecie[0] ?>" alt="zdjęcie">
+                    <h3><?= $zdjecie[1] ?></h3>
+
+                    <?php if ($zdjecie[2] > 40): ?>
+                        <p>Autor: <?= $zdjecie[3] ?> <?= $zdjecie[4] ?>. Wiele osób polubiło ten obraz</p>
+                    <?php else: ?>
+                        <p>Autor: <?= $zdjecie[3] ?> <?= $zdjecie[4] ?>.</p>
+                    <?php endif; ?>
+
+                    <a href="<?= $zdjecie[0] ?>" download>Pobierz</a>
+                </div>
+            <?php endwhile; ?>
+
         </article>
 
         <aside>
             <h2>Najbardziej lubiane</h2>
+
+            <img src="<?= $najbardziej_lubiane[1] ?>" alt="<?= $najbardziej_lubiane[0] ?>">
 
             <strong>Zobacz wszystkie nasze zdjęcia</strong>
         </aside>
