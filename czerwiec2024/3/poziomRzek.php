ab7654e539cb55ae2aabea3a97407627bd533aee
Bartosz <bartoszbachul1@gmail.com>
2025-05-17 11:06:47 +0200

    Skrypt PHP na stronie

--- czerwiec2024/3/poziomRzek.php
+++ czerwiec2024/3/poziomRzek.php
@@ -1,3 +1,44 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'rzeki');
+if (!$conn) {
+    exit("Nie udało się połączyć z bazą danych: " . mysqli_connect_error());
+}
+
+$wynik;
+
+if ($_SERVER['REQUEST_METHOD'] === "POST" && !empty($_POST['stan_rzek'])) {
+    $stan_rzek = $_POST['stan_rzek'];
+    if ($stan_rzek === "wszystkie") {
+        $zapytanie = "SELECT wodowskazy.nazwa, wodowskazy.rzeka, wodowskazy.stanOstrzegawczy, wodowskazy.stanAlarmowy,      pomiary.stanWody
+                    FROM wodowskazy
+                    JOIN pomiary ON pomiary.wodowskazy_id = wodowskazy.id
+                    WHERE pomiary.dataPomiaru = '2022-05-05'";
+
+        $wynik = mysqli_query($conn, $zapytanie);
+    } elseif ($stan_rzek === "ostrzegawczy") {
+        $zapytanie = "SELECT wodowskazy.nazwa, wodowskazy.rzeka, wodowskazy.stanOstrzegawczy, wodowskazy.stanAlarmowy, pomiary.stanWody
+                    FROM wodowskazy
+                    JOIN pomiary ON pomiary.wodowskazy_id = wodowskazy.id
+                    WHERE pomiary.dataPomiaru = '2022-05-05' AND pomiary.stanWody > wodowskazy.stanOstrzegawczy";
+
+        $wynik = mysqli_query($conn, $zapytanie);
+    } elseif ($stan_rzek === "alarmowy") {
+        $zapytanie = "SELECT wodowskazy.nazwa, wodowskazy.rzeka, wodowskazy.stanOstrzegawczy, wodowskazy.stanAlarmowy, pomiary.stanWody
+                    FROM wodowskazy
+                    JOIN pomiary ON pomiary.wodowskazy_id = wodowskazy.id
+                    WHERE pomiary.dataPomiaru = '2022-05-05' AND pomiary.stanWody > wodowskazy.stanAlarmowy";
+
+        $wynik = mysqli_query($conn, $zapytanie);
+    }
+}
+
+
+
+mysqli_close($conn);
+
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 
@@ -23,13 +64,13 @@
 
     <nav>
         <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-            <input type="radio" name="stan-rzek" id="wszystkie">
+            <input type="radio" name="stan_rzek" id="wszystkie" value="wszystkie">
             <label for="wszystkie">Wszystkie</label>
 
-            <input type="radio" name="stan-rzek" id="Ponad stan ostrzegawczy">
+            <input type="radio" name="stan_rzek" id="ostrzegawczy" value="ostrzegawczy">
             <label for="ostrzegawczy">Ponad stan ostrzegawczy</label>
 
-            <input type="radio" name="stan-rzek" id="alarmowy">
+            <input type="radio" name="stan_rzek" id="alarmowy" value="alarmowy">
             <label for="alarmowy">Ponad stan alarmowy</label>
 
             <input type="submit" value="Pokaż">
@@ -49,9 +90,15 @@
                     <th>Aktualny</th>
                 </tr>
 
-                <?php ?>
-
-                <?php ?>
+                <?php while ($stan_rzeki = mysqli_fetch_row($wynik)): ?>
+                    <tr>
+                        <td><?= $stan_rzeki[0] ?></td>
+                        <td><?= $stan_rzeki[1] ?></td>
+                        <td><?= $stan_rzeki[2] ?></td>
+                        <td><?= $stan_rzeki[3] ?></td>
+                        <td><?= $stan_rzeki[4] ?></td>
+                    </tr>
+                <?php endwhile; ?>
             </table>
         </article>
 
