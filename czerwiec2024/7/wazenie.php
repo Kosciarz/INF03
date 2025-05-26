8771c981a277bd6b986155c9116b14d96f24e361
Bartosz <bartoszbachul1@gmail.com>
2025-05-26 14:45:44 +0200

    Struktura strony w HTML

---
+++ czerwiec2024/7/wazenie.php
@@ -0,0 +1,57 @@
+
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ważenie samochodów ciężarowych</title>
+
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    
+    <section>
+        <header>
+            <h1>Ważenie pojazdów we Wrocławiu</h1>
+        </header>
+
+        <header>
+            <img src="obraz1.png" alt="waga">
+        </header>
+    </section>
+
+    <main>
+        <article>
+            <h2>Lokalizacje wag</h2>
+            <ol>
+
+            </ol>
+            <h2>Kontakt</h2>
+            <a href="mailto:wazenie@wroclaw.pl">napisz</a>
+        </article>
+
+        <article>
+            <h2>Alerty</h2>
+            <table>
+                <tr>
+                    <th>rejestracja</th>
+                    <th>ulica</th>
+                    <th>waga</th>
+                    <th>dzień</th>
+                    <th>czas</th>
+                </tr>
+            </table>
+        </article>
+
+        <article>
+            <img src="obraz2.jpg" alt="tir">
+        </article>
+    </main>
+
+    <footer>
+        <p>Stronę wykonał: 0000000000</p>
+    </footer>
+
+</body>
+</html>
