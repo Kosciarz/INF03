96f5d1602dc47e6ad40e395d5aeb3b269362f28d
Bartosz <bartoszbachul1@gmail.com>
2025-05-26 15:02:55 +0200

    Wykonanie skryptu w PHP

--- czerwiec2024/7/wazenie.php
+++ czerwiec2024/7/wazenie.php
@@ -1,4 +1,26 @@
+<?php
 
+$conn = mysqli_connect('localhost', 'root', '', 'wazenietirow');
+
+$zapytanie1 = "SELECT lokalizacje.ulica
+            FROM lokalizacje";
+$lokalizacje = mysqli_query($conn, $zapytanie1);
+
+$zapytanie2 = "SELECT wagi.rejestracja, wagi.waga, wagi.dzien, wagi.czas, lokalizacje.ulica
+            FROM wagi
+            JOIN lokalizacje ON wagi.lokalizacje_id = lokalizacje.id
+            WHERE wagi.waga > 5
+            ORDER BY wagi.czas";
+$wazenia = mysqli_query($conn, $zapytanie2);
+
+$zapytanie3 = "INSERT INTO wagi(wagi.lokalizacje_id, wagi.waga, wagi.rejestracja, wagi.dzien, wagi.czas)
+            VALUES (5, FLOOR(RAND() * 10 + 1), 'DW12345', CURRENT_DATE(), CURRENT_TIME())";
+mysqli_query($conn, $zapytanie3);
+header('Refresh: 10');
+
+mysqli_close($conn);
+
+?>
 
 <!DOCTYPE html>
 <html lang="pl">
@@ -25,7 +47,9 @@
         <article>
             <h2>Lokalizacje wag</h2>
             <ol>
-
+                <?php while ($ulica = mysqli_fetch_row($lokalizacje)): ?>
+                    <li>ulica <?= $ulica[0] ?></li>
+                <?php endwhile; ?>
             </ol>
             <h2>Kontakt</h2>
             <a href="mailto:wazenie@wroclaw.pl">napisz</a>
@@ -41,6 +65,15 @@
                     <th>dzień</th>
                     <th>czas</th>
                 </tr>
+                <?php while ($wazenie = mysqli_fetch_row($wazenia)): ?>
+                    <tr>
+                        <td><?= $wazenie[0] ?></td>
+                        <td><?= $wazenie[4] ?></td>
+                        <td><?= $wazenie[1] ?></td>
+                        <td><?= $wazenie[2] ?></td>
+                        <td><?= $wazenie[3] ?></td>
+                    </tr>
+                <?php endwhile; ?>
             </table>
         </article>
 
