ce7b096bcdacc6aece01b38e9e62cf02548a57b3
Bartosz <bartoszbachul1@gmail.com>
2025-05-28 20:04:05 +0200

    Wykonanie stylów w CSS

---
+++ czerwiec2024/11/peruwianka.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hodowla świnek morskich</title>
+
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    
+    <header>
+        <h1>Hodowla świnek morskich - zamów świnkowe maluszki</h1>
+    </header>
+
+    <section>
+        <article>
+            <nav>
+                <a href="peruwianka.php">Rasa Peruwianka</a>
+                <a href="american.php">Rasa American</a>
+                <a href="crested.php">Rasa Crested</a>
+            </nav>
+            
+            <main>
+                <img src="peruwianka.jpg" alt="Świnka morska rasy peruwianka">
+
+                <hr>
+                <h2>Świnki w tym miocie</h2>
+
+            </main>
+        </article>
+
+        <article>
+            <h3>Poznaj wszystkie rasy świnek morskich</h3>
+            <ol>
+
+            </ol>
+        </article>
+    </section>
+
+    <footer>
+        <p>Stronę wykonał: 00000000000</p>
+    </footer>
+
+</body>
+</html>
