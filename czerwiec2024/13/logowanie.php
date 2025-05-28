fb312233f00f5322a32da6b24667e523b0b7ed45
Bartosz <bartoszbachul1@gmail.com>
2025-05-28 22:32:19 +0200

    Wykonanie struktury strony

---
+++ czerwiec2024/13/logowanie.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forum o psach</title>
+
+    <link rel="stylesheet" href="styl4.css">
+</head>
+<body>
+    
+    <header>
+        <h1>Forum o psach</h1>
+    </header>
+
+    <main>
+        <section>
+            <img src="obraz.jpg" alt="foksterier">
+        </section>
+
+        <section>
+            <article>
+                <h2>Zapisz się</h2>
+                <form action="#" method="POST">
+                    <label for="login">login: </label>
+                    <input type="text" name="login" id="login">
+
+                    <label for="haslo">hasło: </label>
+                    <input type="password" name="haslo" id="haslo">
+
+                    <label for="powtorz-haslo">powtórz hasło: </label>
+                    <input type="password" name="powtorz-haslo" id="powtorz-haslo">
+
+                    <input type="submit" value="Zapisz">
+                </form>
+            </article>
+
+            <article>
+                <h2>Zapraszamy wszystkich</h2>
+                <ol>
+                    <li>właścicieli psów</li>
+                    <li>weterynarzy</li>
+                    <li>tych, co chcą kupić psa</li>
+                    <li>tych, co lubią psy</li>
+                </ol>
+                <a href="regulamin.html">Przeczytaj regulamin forum</a>
+            </article>
+
+        </section>
+    </main>
+
+    <footer>
+        Stronę wykonał: 00000000000
+    </footer>
+
+</body>
+</html>
