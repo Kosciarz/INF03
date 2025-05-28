ceec3736bfcd362a277cd4604e4bb539ab8e057b
Bartosz <bartoszbachul1@gmail.com>
2025-05-28 20:17:45 +0200

    Wykonanie skryptu w PHP

--- czerwiec2024/11/crested.php
+++ czerwiec2024/11/crested.php
@@ -1,3 +1,27 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'hodowla');
+
+$zapytanie1 = "SELECT rasy.rasa
+        FROM rasy";
+$rasy = mysqli_query($conn, $zapytanie1);
+
+$zapytanie2 = "SELECT DISTINCT swinki.data_ur, swinki.miot, rasy.rasa
+            FROM swinki
+            JOIN rasy ON swinki.rasy_id = rasy.id
+            WHERE rasy.id = 7";
+$glowna_swinka = mysqli_fetch_row(mysqli_query($conn, $zapytanie2));
+
+$zapytanie3 = "SELECT swinki.imie, swinki.cena, swinki.opis
+            FROM swinki
+            JOIN rasy ON swinki.rasy_id = rasy.id
+            WHERE rasy.id = 7";
+$swinki = mysqli_query($conn, $zapytanie3);
+
+mysqli_close($conn);
+
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -22,18 +46,27 @@
             </nav>
             
             <main>
-                <img src="peruwianka.jpg" alt="Świnka morska rasy peruwianka">
+                <img src="crested.jpg" alt="Świnka morska rasy peruwianka">
+                <h2>Rasa: <?= $glowna_swinka[2] ?></h2>
+                <p>Data urodzenia: <?= $glowna_swinka[0] ?></p>
+                <p>Oznaczenie miotu: <?= $glowna_swinka[1] ?></p>
 
                 <hr>
                 <h2>Świnki w tym miocie</h2>
 
+                <?php while ($swinka = mysqli_fetch_row($swinki)): ?>
+                    <h3><?= $swinka[0] ?> - <?= $swinka[1] ?> zł</h3>
+                    <p><?= $swinka[2] ?></p>
+                <?php endwhile; ?>
             </main>
         </article>
 
         <article>
             <h3>Poznaj wszystkie rasy świnek morskich</h3>
             <ol>
-
+                <?php while ($rasa = mysqli_fetch_row($rasy)): ?>
+                    <li><?= $rasa[0] ?></li>
+                <?php endwhile; ?>
             </ol>
         </article>
     </section>
